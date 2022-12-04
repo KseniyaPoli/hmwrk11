803246d697ace961c5b39549905def130b580930
Polityukova, Kseniya <Kseniya.Polityukova@nedra.digital>
2022-12-04 15:14:03 +0300

    fixed comments

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -1,13 +1,33 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demoqa.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
 
     @BeforeAll
     static void configure() {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1800x1028";
+    }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
